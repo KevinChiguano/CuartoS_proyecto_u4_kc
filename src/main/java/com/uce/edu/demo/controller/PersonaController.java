162fd53973bcf7eda69d4c96dbaa5127c9d2959d
KevinChiguano <kevin_chili1998@hotmail.com>
2022-09-05 20:47:07 -0500

    verbos POST PUT GET DELETE

--- src/main/java/com/uce/edu/demo/controller/PersonaController.java
+++ src/main/java/com/uce/edu/demo/controller/PersonaController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uce.edu.demo.repository.modelo.Persona;
@@ -26,7 +30,41 @@ public class PersonaController {
 		
 		modelo.addAttribute("personas",lista);
 		
-		return "vistaListaEstudiantes";
+		return "vistaListaPersonas";
 	}
+	
+	@GetMapping("/buscarUno/{idPersona}")
+	public String buscarPersona(@PathVariable("idPersona") Integer id, Model model) {
+		System.out.println("El ID: "+id);
+		Persona per = this.personaService.buscarPorId(id);
+		model.addAttribute("persona", per);
+		return "vistaPersona";
+	}
+	
+	@PutMapping("/actualizar/{idPersona}")
+	public String actualizarPersona(@PathVariable("idPersona") Integer id, Persona persona) {
+		persona.setId(id);
+		this.personaService.actualizar(persona);
+		return "redirect:/personas/buscar";
+	}
+	
+	@DeleteMapping("/eliminar/{idPersona}")
+	public String borrarPersona(@PathVariable("idPersona") Integer id) {
+		this.personaService.eliminar(id);
+		return "redirect:/personas/buscar";
+	}
+	
+	@PostMapping("/insertar")
+	public String insertarPersona(Persona persona) {
+		this.personaService.guardar(persona);
+		return "redirect:/personas/buscar";
+	}
+	
+	//Metodos de redireccionamientos a paginas
+	@GetMapping("/nuevaPersona")
+	public String paginaNuevaPersona(Persona persona) {
+		return "vistaNuevaPersona";
+	}
+	
 
 }
